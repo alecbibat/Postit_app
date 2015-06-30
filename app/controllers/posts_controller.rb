a769f9ec897618c4dc96eb3656ccf69c38d9324e
Alec Bibat <alecbibat@gmail.com>
2015-06-29 18:12:34 -0600

    list comments under post on show route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,11 @@ class PostsController < ApplicationController
   end
 
   def show
+
+    @post = Post.find(params[:id])
+
     # this should handle clicking on a post from the homepage
+
+    # this should display comments from the specific post it was displayed under
   end
 end
