2b5853904e78cc7c8980ea17ecb6045299f2c84c
Alec Bibat <alecbibat@gmail.com>
2015-06-30 06:40:03 -0600

    add new post on nav bar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,24 @@ class PostsController < ApplicationController
 
     # this should display comments from the specific post it was displayed under
   end
+
+  def new
+    @post = Post.new()
+
+    # display new post form
+  end
+
+  def create
+    # handle new post form submit
+  end
+
+  def edit
+    # display edit post form
+  end
+
+  def update
+    # handle edit post form submit
+  end
+
+
 end
