826594cfe6eaf3937e4ead92e91386f572906e2a
Alec Bibat <alecbibat@gmail.com>
2015-07-01 06:43:34 -0600

    set up categories

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,29 @@
+class CategoriesController < ApplicationController
+
+# index
+# show
+# new
+# create
+# edit
+# update
+
+def index
+  @category = Category.all
+end
+
+def show
+end
+
+def new
+end
+
+def create
+end
+
+def edit
+end
+
+def update
+end
+
+end
