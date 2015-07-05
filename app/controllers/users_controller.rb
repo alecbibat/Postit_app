8d222137a7bbb7c790f0dab7f7f27b20a903a58a
Alec Bibat <alecbibat@gmail.com>
2015-07-05 14:51:38 -0600

    setup_user_mvc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
-class UsersController < ActiveRecord::Base
+class UsersController < ApplicationController
+
+def new
+end
+
+def create
+end
+
+def edit
+end
+
+def update
+end
+
   # index?
   # new : register page
   # create : register
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
+  get '/register', to: 'users#new'
+
+  # put sessions resources here
+  # new, create, destroy
+
   resources :posts, except:[:destroy] do
     resources :comments, except:[:destroy]
   end
