b3a661e7b112dda18b63bde565c09cc84f06a0a8
Alec Bibat <alecbibat@gmail.com>
2015-07-04 21:30:28 -0600

    fix_datetime

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,17 @@
 module ApplicationHelper
+
+  # takes a url string
+  # appends "http://"
   def fix_url(str)
     if str.starts_with? "http://"
     else
       "http://" + str
     end
+  end
+
+  # takes a datetime object
+  # standardizes and returns string
+  def datetime(dt)
+    dt.strftime("%k:%M %m/%d/%y %Z")
   end
 end
