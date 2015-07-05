57a26ef8685ce7bc29b457045bfbc44ed69e5f65
Alec Bibat <alecbibat@gmail.com>
2015-07-05 15:33:16 -0600

    start users_controller_create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
 
 def new
-  binding.pry
 end
 
 def create
+  binding.pry
+  @user = User.new(user_params)
+  if @user.save
+    flash[:notice] = "You've successfully registered!"
+    redirect_to posts_path
+  else
+    render :new
+  end
 end
 
 def edit
@@ -18,4 +25,10 @@ end
   # create : register
   # edit : edit page
   # update : handle update action
+end
+
+private
+
+def user_params
+  params.require(:user).permit!
 end
