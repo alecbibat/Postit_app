b75bd6e3aed5a71fd2cd2e317ac06a3d6a96fad9
Alec Bibat <alecbibat@gmail.com>
2015-07-05 18:47:59 -0600

    work on log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if User.find_by(username: (params[:username]))
+      @user = User.find_by(username: (params[:username]))
+      @user.authenticate(params[:password])
+      @user = session[:user]
+      flash[:notice] = "You have successfully logged in!"
+      redirect_to root_path
+
+    else
+      flash[:error] = "There's something wrong with your username or password."
+      render :create
+    end
   end
 
   def destroy
