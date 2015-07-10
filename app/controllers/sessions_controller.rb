6defa1953a1d16615954d761ea6c30a6d51b25ab
Alec Bibat <alecbibat@gmail.com>
2015-07-10 13:06:07 -0600

    add current_user applicationhelper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,10 +10,10 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: (params[:username]))
     if @user && @user.authenticate(params[:password])
       session[:user] = @user
-      flash[:notice] = "You have successfully logged in as #{@user.username}!"
-      redirect_to root_path
       params.delete :username
       params.delete :password
+      flash[:notice] = "You have successfully logged in as #{current_user.username}!"
+      redirect_to root_path
       binding.pry
     else
       flash[:error] = "There's something wrong with your username or password."
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,5 +17,5 @@ module ApplicationHelper
 end
 
 def current_user
-  @current_user ||= User.find_by(id: session[:user_id])
+  @current_user ||= User.find_by(id: session[:user].id)
 end
